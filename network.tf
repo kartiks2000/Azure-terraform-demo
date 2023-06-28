691cd95a7e32824aab501c2da8aeef4037ce50a7
kartik saxena <kartiksaxena@kartiks-MacBook-Pro.local>
2023-06-28 15:06:35 -0400

    test

--- network.tf
+++ network.tf
@@ -7,6 +7,18 @@ resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "Allow tcp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "example" {
